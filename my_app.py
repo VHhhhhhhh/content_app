48a3880ec60b8f0b4e269bf1407ff78e0529655a
Vjosa Hajdini <vjosahajdini@Vjosas-Air.fritz.box>
2023-05-20 11:09:52 +0200

    Resolve merge conflicts

--- my_app.py
+++ my_app.py
@@ -18,7 +18,7 @@ if st.button('Generate'):
     for i in range(number_of_contents):
         content, tokens_used = generate_content(title, keywords, avoid_keywords, content_type, length, number_of_contents, service_areas, api_key)
         content_length = len(content.split())
-        st.write(f"{i+1}. ({content_length} words)\n{content}\n")
+        st.markdown(f"**{i+1}. ({content_length} words)**\n{content}\n")
         total_length += content_length
         total_tokens += tokens_used
 
