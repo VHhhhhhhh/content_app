d9283040bf7e15c826264b1a92e889c35d065355
Your Name <vjossahl@gmail.com>
2024-03-06 13:11:28 +0100

    Remove main.py and requirements1.txt

--- main.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-
-driver = webdriver.Chrome('./Desktop/VJOSA_Test.code-workspace/chromedriver_mac_arm64')
-
-driver.get('https://seoreports.agency/admin')
-
-
